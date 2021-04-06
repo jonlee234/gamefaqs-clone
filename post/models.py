7761b22aab634541d92fb10358515087d44c3b59
Jonathane Leavell <jonathaneleavell@gmail.com>
2021-04-06 13:45:05 -0400

    MADE PLATFORM A CHOICE FIELD, UPDATED POSTING FIEL

--- post/models.py
+++ post/models.py
@@ -1,12 +1,32 @@
 from django.db import models
+from accounts.models import CustomUser
 
 # Create your models here.
-
+PLATFORM_CHOICES = (
+    ("1", "PC"),
+    ("2", "PS5"),
+    ("3", "XSX"),
+    ("4", "Switch"),
+    ("5", "iOS"),
+    ("6", "Android"),
+    ("7", "Arcade"),
+    ("8", "PS4"),
+    ("9", "PS3"),
+    ("10", "Xbox One"),
+    ("11", "Xbox 360"),
+    ("12", "Sega"),
+    ("13", "Wii U"),
+    ("14", "Wii"),
+    ("15", "PSP"),
+    ("16", "Vita"),
+    ("17", "3DS"),
+    ("18", "RetoPy"),
+    ("19", "Other Systems"),
 
 class Post(models.Model):
     text = models.CharField(max_length=500)
     topic = models.CharField(max_length=40)
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
-    user_posted = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_posted = models.ForeignKey(CustomUser on_delete=models.CASCADE)
     thumbnail = models.ImageField()
-    # platforms = models.ManyToManyField()
+    platforms = models.CharField(choices= PLATFORM_CHOICES max_length=50)
