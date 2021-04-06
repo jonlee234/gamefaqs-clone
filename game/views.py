683266196696e4209cbaab9f665291abc05a845d
codenameGIZMO <shavonnecarson94@gmail.com>
2021-04-05 19:12:08 -0700

    set up game model and all game view

--- game/views.py
+++ game/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from game.models import Game
 
 # Create your views here.
+
+
+def AllGameView(request):
+    games = Game.objects.all()
+    template = 'all-games.html'
+    return render(request, template, {
+        'games': games
+    })
