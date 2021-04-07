c6be3e120020d83b10484345831a0f1ad6dab5f6
Robert Havelaar <RMHavelaar@gmail.com>
2021-04-07 12:13:57 -0500

    Made settings.py database a str.

--- gamefaqs_project/settings.py
+++ gamefaqs_project/settings.py
@@ -82,9 +82,9 @@ WSGI_APPLICATION = "gamefaqs_project.wsgi.application"
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': str(BASE_DIR / "db.sqlite3"),
     }
 }
 
