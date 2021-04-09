4d491cc015d72c00cc5c95cfe7109c0be2900abc
Jonathane Leavell <jonathaneleavell@gmail.com>
2021-04-09 10:42:12 -0400

    added login required Decorator

--- post/views.py
+++ post/views.py
@@ -4,12 +4,13 @@ from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from post.models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 # Create your views here.
 
 
-class PostCreate(CreateView):
+class PostCreate(LoginRequiredMixin, CreateView):
     template_name = "add_post.html"
     model = Post
     fields = ["topic", "game", "platforms", "text", "post_date", "thumbnail"]
