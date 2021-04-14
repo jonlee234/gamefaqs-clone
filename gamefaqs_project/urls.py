a59799bf931ef3a29dca852f5a56fc7775fb3ca4
Robert Havelaar <RMHavelaar@gmail.com>
2021-04-14 18:28:59 -0500

    Names added to all urls that didnt have them

--- gamefaqs_project/urls.py
+++ gamefaqs_project/urls.py
@@ -13,6 +13,7 @@ from accounts.views import (
     unfollow_view,
     user_profile_view,
     favorite_game_view,
+    user_list_view,
 )
 from game.views import (
     AddGameView,
@@ -36,7 +37,7 @@ from post.views import (
 urlpatterns = [
     # Core links
     path("", user_view.index, name="homepage"),
-    path("admin/", admin.site.urls),
+    path("admin/", admin.site.urls, name="admin"),
     # Game Related Links
     path("viewGames/", AllGameView.as_view(), name="games"),
     path("viewGames/<int:game_id>/", GameTitleView, name="game-title"),
@@ -44,23 +45,24 @@ urlpatterns = [
     path("viewGames/platform/<int:platform>/", PlatformView, name="platform"),
     path("viewGames/search/<str:title>/", Search, name="search"),
     # Auth Links
-    path("login/", auth_view.login_view),
-    path("signup/", auth_view.signup_view),
-    path("logout/", auth_view.logout_view),
+    path("login/", auth_view.login_view, name="login"),
+    path("signup/", auth_view.signup_view, name="signup"),
+    path("logout/", auth_view.logout_view, name="logout"),
     # Post Links
     path("post/add/", PostCreate.as_view(), name="post-create"),
     path("posts/<int:pk>/", PostDetailView.as_view(), name="post_detail"),
     path("posts/", PostListView.as_view(), name="post-list"),
     path("post/<int:pk>/comment/", add_comment_to_post, name="add_comment_to_post"),
     # Follow Links
-    path("follow/<int:user_id>", follower_view),
-    path("unfollow/<int:user_id>", unfollow_view),
+    path("follow/<int:user_id>", follower_view, name="follow"),
+    path("unfollow/<int:user_id>", unfollow_view, name="unfollow"),
     # User/Profile Links
-    path("user/<int:CustomUser_id>/", user_profile_view),
+    path("user/<int:CustomUser_id>/", user_profile_view, name="user-profile"),
     path("viewProfile/<str:username>/", user_profile_view, name="profile"),
+    path("viewUsers/", user_list_view, name="users"),
     # Favorites
-    path("favorite_game/<int:game_id>", favorite_game_view),
-    path("unfavorite_game/<int:game_id>", unfavorite_game_view),
+    path("favorite_game/<int:game_id>", favorite_game_view, name="favorite_game"),
+    path("unfavorite_game/<int:game_id>", unfavorite_game_view, name="unfavorite_game"),
     # Statics
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
